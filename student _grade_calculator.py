9c08a3a70efaa64f4032e6ba74586c3427137bec
Priya1992-wq <panditpriya958@gmail.com>
2026-07-26 21:02:13 +0530

    Create student _grade_calculator.py

---
+++ student _grade_calculator.py
@@ -0,0 +1,23 @@
+# student Grade Calcualator project
+# Author: Priya Pandit
+#Variable
+student1=96
+student2=31
+student3=31
+#calcualtion
+total= student1+student2+student3
+average= total /3
+#Condition+Indentation
+if average>80:
+  grade="A"
+  print("Grade:A- Excellent!")
+elif average>=60:
+  grade="B"
+  print("Grade:B- Good Job!")
+else:
+  grade="C"
+  print("Grade:C-Need to improvement")
+  #output
+print("Total Marks:",total)
+print("Average:",round(average,2)
+
